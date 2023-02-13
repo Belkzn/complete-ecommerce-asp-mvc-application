f479ab23f2eafd1fffaded5aa8fe7c148cb63185
olcayturkoglu <70755738+olcayturkoglu@users.noreply.github.com>
2023-02-13 12:15:33 +0100

    Add project files.

---
+++ Betulstest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Betulstest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
